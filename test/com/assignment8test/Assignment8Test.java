7746d26e43f7f2372ef5bb43a950fedeecc16655
William Cocomise <89233920+b5yB@users.noreply.github.com>
2022-01-10 16:13:50 -0600

    v4 ("thread-safe")

--- test/com/assignment8test/Assignment8Test.java
+++ test/com/assignment8test/Assignment8Test.java
@@ -1,17 +1,25 @@
 	package com.assignment8test;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 
 import org.junit.jupiter.api.Test;
 
@@ -19,62 +27,54 @@ import com.assignment8.Assignment8;
 
 public class Assignment8Test {
 	
-/*
-	@Test
-    public void getData () {
+	//Not sure if placing 
+	public static List<Integer> allNum = Collections.synchronizedList(new ArrayList<>(1000));
 
-        Assignment8 assignment = new Assignment8();
-       
-
-        for (int i=0; i<1000; i++) {
-
-            List<Integer> numbersList = assignment.getNumbers();
-
-            System.out.println(numbersList);
-
-        }
-
-    }
-*/
 	
-	@Test
-	public void getDataAsync () {
+	public static void main (String[] args) {
 		
 		Assignment8 a8 = new Assignment8();
 		
-		ExecutorService eServIO = Executors.newCachedThreadPool();
-		
 		//Thought I needed fixedThreadPool as a crutch for my old machine... but cachedThreadPool works fine!  
 		//ExecutorService eServCPU = Executors.newFixedThreadPool(5);
-
-		List<CompletableFuture<Void>> allBatch = new ArrayList<>();
-		List<Integer> allNum = new ArrayList<>();
-		Map<Integer, Long> count = new HashMap<>();
+		ExecutorService eServIO = Executors.newCachedThreadPool();
 		
+		List<CompletableFuture<Void>> allBatch = new ArrayList<>(1000);
+		
+		Map<Integer, Long> result = new ConcurrentHashMap<>();
+				
 		for(int i=0; i<1000; i++) {
 			CompletableFuture<Void> batch = CompletableFuture
 															 .supplyAsync(() -> a8.getNumbers(), eServIO)
-															 .thenAcceptAsync(numList -> allNum.addAll(numList));
-				
+															 .thenAccept(numList -> toAllNum(numList));
 			
-			
-			allBatch.add(batch);			
+			allBatch.add(batch);
 		}
 	
 		while(allBatch.stream()
 				   .filter(CompletableFuture::isDone)
 				   .count() < 1000){
-					   
+						try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
 				   }
 			
-		//System.out.println("Total elements: " + allNum.size());
-		
-		Map<Integer, Long> result = allNum.stream()
-			  .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		System.out.println("Total elements: " + allNum.size());
 
-		//System.out.println("Unique elements: " + result.size());
-		System.out.println(result.toString().replace("{", "").replace("}", ""));
+		result = allNum.stream()
+					   .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));			
 		
+
+		
+		System.out.println("Unique elements: " + result.size());
+		System.out.println(result.toString().replace("{", "").replace("}", ""));
 	}
 
+	
+	public static List<Integer> toAllNum (List<Integer> numList) {
+		try { Thread.sleep(500); } catch (InterruptedException e) { e.printStackTrace(); }
+	    synchronized (numList) {
+			 allNum.addAll(numList); 
+			 }
+	    return allNum;
+	}
+	
 }
